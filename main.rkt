e5dabc29be51cfc653481e7062764ff06feafc22
Nikolai Kozak <nikokozak@gmail.com>
2021-07-15 23:11:48 -0400

    coerce OpenGL version, require OpenGL, add run loop

--- main.rkt
+++ main.rkt
@@ -1,9 +1,33 @@
 #lang racket
 
 (require glfw3)
+(require opengl)
 
 (glfwInit)
+
+;; Try to force a more recent version of OpenGL than the Compatibility 2.1 version in MacOS
+(glfwWindowHint GLFW_OPENGL_FORWARD_COMPAT GL_TRUE)
+(glfwWindowHint GLFW_OPENGL_PROFILE GLFW_OPENGL_CORE_PROFILE)
+(glfwWindowHint GLFW_CONTEXT_VERSION_MAJOR 3)
+(glfwWindowHint GLFW_CONTEXT_VERSION_MINOR 3)
+
 (define window (glfwCreateWindow 800 600 "example window" #f #f))
-(sleep 3)
-(glfwDestroyWindow window)
-(glfwTerminate)
+
+;; Ensure the context is correct
+(glfwMakeContextCurrent window)
+
+; (display (glfwGetVersionString))
+
+;; Our run loop
+(define (run window-pointer)
+  (if (= (glfwWindowShouldClose window-pointer) 1)
+      (end window-pointer)
+      (run window-pointer)))
+
+;; Our kill function
+(define (end window-pointer)
+  (glfwDestroyWindow window-pointer)
+  (glfwTerminate))
+
+;; Run the program
+(run window)
